a8e378840eb47bd36ff5c7d8d49fc0d47d720a68
valentin <valenp97@gmail.com>
2023-08-22 22:28:08 +0200

    New endpoint for creating transactions.

--- api/routers/transaction_router.py
+++ api/routers/transaction_router.py
@@ -1,10 +1,30 @@
+from datetime import datetime
+
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
+from starlette import status
 
 from database import db_dependency
 from models import Transaction
 
 router = APIRouter(prefix='/transactions')
 
+
+class TransactionModel(BaseModel):
+    payee: str = Field(min_length=2)
+    creation_date: datetime = Field(default=datetime.now())
+    transaction_date: datetime
+    description: str = Field(min_length=2, max_length=100)
+    amount: float = Field()
+
+
 @router.get('/all')
 async def read_all_transactions(db: db_dependency):
     return db.query(Transaction).all()
+
+
+@router.post('/create', status_code=status.HTTP_201_CREATED)
+async def create_new_transaction(db: db_dependency, transaction_request: TransactionModel):
+    transaction_model = Transaction(**transaction_request.dict())
+    db.add(transaction_model)
+    db.commit()
