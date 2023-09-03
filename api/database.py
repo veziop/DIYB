f83d730eb83c2abf510ee71ce5d40a1c122edcba
valentin <valenp97@gmail.com>
2023-09-04 00:48:26 +0200

    Intalled and applied black autoformatter

--- api/database.py
+++ api/database.py
@@ -10,14 +10,11 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-DATABASE_URL = 'sqlite:///../diyb.db'
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+DATABASE_URL = "sqlite:///../diyb.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
@@ -25,5 +22,6 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 db_dependency = Annotated[Session, Depends(get_db)]
