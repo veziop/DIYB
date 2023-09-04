a582896365b2ef7eff8776a5fbf2bd6d074e1100
valentin <valenp97@gmail.com>
2023-09-05 00:00:48 +0200

    Edited the <amount> column datatype; edited the datatime columns too

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@ description: Module for the definitions all the project's models.
 from datetime import datetime
 
 from database import Base
-from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey, Numeric, Date
 
 
 class Transaction(Base):
@@ -19,35 +19,29 @@ class Transaction(Base):
         doc="Unique identifier of the transaction entry",
     )
     payee = Column(String, name="payee", doc="Name/title of the payee")
-    creation_date = Column(
+    creation_datetime = Column(
         DateTime,
         default=datetime.now,
-        doc="Date/time of creation of the transaction entry",
+        doc="Date/time of creation of the transaction entry in the database",
     )
-    last_update_date = Column(
+    last_update_datetime = Column(
         DateTime,
         default=datetime.now,
         doc="Date/time of the last update operation of the transaction entry",
     )
     transaction_date = Column(
-        DateTime,
-        doc="Date/time of the transaction between the user and the payee",
+        Date,
+        doc="Date of the transaction between the user and the payee",
     )
     description = Column(
         String,
         default="no description",
         doc="User defined description of the transaction entry",
     )
-    amount_cents = Column(
-        Integer,
-        doc="""Transaction amount in cents; 
-        positive for inflow, negative for outflow;
-        ie amount_cents of -1450 represent -14.50€""",
-    )
-    amount_whole = Column(
-        Float,
-        doc="""Transaction amount in euros; 
-        this column is generated from the user's input in <ammount_cents>""",
+    amount = Column(
+        Numeric(10, 2),
+        name="amount",
+        doc="""Transaction amount in euros, with a precision of two decimal places""",
     )
 
 
--- api/routers/transaction_router.py
+++ api/routers/transaction_router.py
@@ -4,7 +4,8 @@ author: Valentin Piombo
 email: valenp97@gmail.com
 description: Module for the definitions of routes related to the Transaction model.
 """
-from datetime import datetime
+from datetime import datetime, date
+from decimal import Decimal
 
 from fastapi import APIRouter, HTTPException, Path
 from pydantic import BaseModel, Field
@@ -18,10 +19,10 @@ router = APIRouter(prefix="/transactions")
 
 # Data verification using pydantic
 class TransactionRequest(BaseModel):
-    payee: str = Field(min_length=2)
-    transaction_date: datetime = Field(default=datetime.now())
-    description: str = Field(min_length=2, max_length=100)
-    amount_cents: int = Field()
+    payee: str = Field(min_length=1)
+    transaction_date: date = Field(default=date.today())
+    description: str = Field(max_length=100)
+    amount: Decimal = Field(decimal_places=2)
 
 
 @router.get("/all", status_code=status.HTTP_200_OK)
@@ -31,8 +32,10 @@ async def read_all_transactions(db: db_dependency):
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_new_transaction(db: db_dependency, transaction_request: TransactionRequest):
-    transaction_model = Transaction(**transaction_request.dict())
-    transaction_model.amount_whole = transaction_request.amount_cents / 100
+    transaction_request_data = transaction_request.dict()
+    transaction_request_data["creation_datetime"] = datetime.now().replace(microsecond=0)
+    transaction_request_data["last_update_datetime"] = datetime.now().replace(microsecond=0)
+    transaction_model = Transaction(**transaction_request_data)
     db.add(transaction_model)
     db.commit()
 
@@ -56,8 +59,8 @@ async def update_one_transaction(
         raise HTTPException(status_code=404, detail="Transaction not found")
     transaction_model.payee = transaction_request.payee
     transaction_model.transaction_date = transaction_request.transaction_date
+    transaction_model.last_update_datetime = datetime.now().replace(microsecond=0)
     transaction_model.description = transaction_request.description
-    transaction_model.amount_cents = transaction_request.amount_cents
-    transaction_model.amount_whole = transaction_request.amount_cents / 100
+    transaction_model.amount = transaction_request.amount
     db.add(transaction_model)
     db.commit()
