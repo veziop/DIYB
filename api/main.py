fdd51da2f3957de93bb764e445a85db841f54f26
valentin <valenp97@gmail.com>
2023-09-20 00:01:47 +0200

    Included the balance router in the fastapi app

--- api/main.py
+++ api/main.py
@@ -9,8 +9,10 @@ from fastapi import FastAPI
 from database import engine
 import models
 from routers.transaction_router import router as transaction_router
+from routers.balance_router import router as balance_router
 
 app = FastAPI()
 app.include_router(transaction_router)
+app.include_router(balance_router)
 
 models.Base.metadata.create_all(bind=engine)
