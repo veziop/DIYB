6503b6ede20629b546c2b8a77e36508d9690ee18
valen <valenp97@gmail.com>
2023-11-26 23:18:44 +0100

    1: moved main.py to the project root

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ author: Valentin Piombo
 email: valenp97@gmail.com
 description: Project's root module.
 """
+import uvicorn
 from fastapi import FastAPI
 
-import models
-from database import engine
-from routers.balance_router import router as balance_router
-from routers.transaction_router import router as transaction_router
+from api import models
+from api.database import engine
+from api.routers.balance_router import router as balance_router
+from api.routers.transaction_router import router as transaction_router
 
 app = FastAPI(
     title="DIYB",
@@ -25,3 +26,10 @@ app.include_router(transaction_router)
 app.include_router(balance_router)
 
 models.Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port="8080",
+    )
