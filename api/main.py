bab11af4c46f9fae46e19d2121c08a8bf688ac54
valen <valenp97@gmail.com>
2023-11-26 21:45:55 +0100

    1: added fastapi app parameters

--- api/main.py
+++ api/main.py
@@ -11,7 +11,16 @@ from database import engine
 from routers.balance_router import router as balance_router
 from routers.transaction_router import router as transaction_router
 
-app = FastAPI()
+app = FastAPI(
+    title="DIYB",
+    version="0.1.0",
+    summary="Do It Yourself Budget - a personal finance organizer",
+    contact={
+        "author": "Valentin Piombo",
+        "email": "valenp97@gmail.com",
+        "url": "https://github.com/veziop/DIYB",
+    },
+)
 app.include_router(transaction_router)
 app.include_router(balance_router)
 
