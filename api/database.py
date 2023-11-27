1b2baafe2e837ca1f57e945c8462abbebe164765
valen <valenp97@gmail.com>
2023-11-27 23:19:06 +0100

    1: modified the sqlite db path

--- api/database.py
+++ api/database.py
@@ -10,7 +10,7 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///../diyb.db"
+DATABASE_URL = "sqlite:///./diyb.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
