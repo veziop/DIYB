a138c8cdcd4f88c9a680790983bff453449463fe
valen <valenp97@gmail.com>
2024-01-28 18:49:45 +0100

    2: bugfix in attribute lookup

--- api/database.py
+++ api/database.py
@@ -26,7 +26,7 @@ def get_db():
         db.rollback()
         raise HTTPException(
             status_code=e.status_code,
-            detail={"message": e.detail.message},
+            detail={"message": e.detail},
         )
     finally:
         db.close()
