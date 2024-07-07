e318f5a92ec17bae125aa06f4758a9fe6c2bd565
valen <valenp97@gmail.com>
2024-07-07 22:05:22 +0200

    7: changed the database URL so to map a persistant volume in the docker host

--- api/database.py
+++ api/database.py
@@ -12,7 +12,7 @@ from fastapi import Depends, HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, scoped_session, sessionmaker
 
-DATABASE_URL = "sqlite:///./diyb.db"
+DATABASE_URL = "sqlite:///./data/diyb.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
